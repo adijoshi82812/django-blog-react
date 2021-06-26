7f2266dd6650c05b498ca6254cb276f17a85b1ae
= <=>
2021-06-26 20:36:55 +0530

    Created views for contact app
    This view only accepts post requests, also added urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('contact/', include('contact.urls')),
     path('daily_quotes/', include('dailyquote.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
