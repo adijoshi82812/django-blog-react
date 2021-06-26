3326ed640c15c302f7012e3880ed5e88bc6fd274
= <=>
2021-06-26 19:49:44 +0530

    Created daily quote model
    This model consits of quote(textfield), author(charfield) and pub_date which is the date published, also migrated this model to the database and registed this model to the admin site

--- backend/dailyquote/models.py
+++ backend/dailyquote/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class DailyQuote(models.Model):
+    quote = models.TextField()
+    author = models.CharField(max_length=20)
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.author
