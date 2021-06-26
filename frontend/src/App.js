a9a32fdf352752174e7d0963657cbcff4f85c631
= <=>
2021-06-26 19:23:47 +0530

    Added loading animate
    This animation is not dynamic, it just generates a random number from 1000 to 1500(which is 1 to 1.5 seconds in time), and display loading screen until the timer is not zero. So in short the user can get an illusion of loading screen thinking the page is actually loading where actually the page will load after the loading screen is set to false

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,37 @@ import React, { Component } from 'react';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 
+import loading from './loading.gif';
+
 class App extends Component{
+  state = {
+    loading: true
+  };
+
+  setLoading = () => {
+    this.setState({ loading: false });
+  };
+
+  componentDidMount(){
+    setTimeout(this.setLoading, Math.floor(Math.random() * 1500 -1000) + 1000);
+  }
+
   render(){
+    if(this.state.loading){
+      return(
+        <div className="w3-display-container" style={{ height: "100vh", width: "100vw" }}>
+          
+          <div className="w3-display-middle">
+            
+            <img src={loading} alt="Loading"/>
+            <br/>
+            Loading...
+
+          </div>
+        </div>
+      );
+    }
+
     return(
       <div>
         <Nav/>
