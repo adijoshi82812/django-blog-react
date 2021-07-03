d7a174d7bc869774e80a447685d41720beb728df
adijoshi82812 <adijoshi82812@gmail.com>
2021-07-04 02:44:23 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,17 @@
 import React, { Component } from 'react';
 
-import { Route } from 'react-router-dom';
-
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import BlogRender from './components/BlogRender';
-
-import loading from './loading.gif';
-
-import axios from 'axios';
 
 class App extends Component {
-  state = {
-    loading: true,
-    linksData: [],
-  };
-
-  setLoading = () => {
-    this.setState({ loading: false });
-  };
-
-  componentDidMount() {
-    axios.get('http://localhost:8000/blogs/')
-      .then(res => this.setState({ linksData: res.data }));
-
-    setTimeout(this.setLoading, Math.floor(Math.random() * 1500 - 1000) + 1000);
-  }
-
   render() {
-    const links = this.state.linksData.map(data => {
-      return (
-        <Route exact path={"/blog/" + data.slug}>
-          <BlogRender />
-        </Route>
-      );
-    });
-
-    if (this.state.loading) {
-      return (
-        <div className="w3-display-container" style={{ height: "100vh", width: "100vw" }}>
-
-          <div className="w3-display-middle">
-
-            <img src={loading} alt="Loading" />
-            <br />
-            Loading...
-
-          </div>
-        </div>
-      );
-    }
-
     return (
       <div>
-        <Nav links={links}/>
+
+        <Nav />
+
         <Footer />
+
       </div>
     );
   }
